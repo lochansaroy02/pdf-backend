9627f8047aa2a4cb1e3a622b3d6fc0b837ac314c
lochansaroy02 <lochan.kumar.fit.csebct20@teamfuture.in>
2024-11-05 20:37:03 +0530

    created aws-s3 upload

--- app/api/endpoints/upload.py
+++ app/api/endpoints/upload.py
@@ -3,11 +3,10 @@ from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import PDFDocument
-from app.utils.pdf_processing import extract_text_from_pdf
 import boto3
-import os  # Import os module
+import os
 from datetime import datetime
-from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import NoCredentialsError, ClientError
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -26,9 +25,10 @@ s3_client = boto3.client(
 BUCKET_NAME = os.getenv("AWS_S3_BUCKET")
 
 
+
 @router.get("/upload")
 async def root():
-    return {"message": "Hello from FastAPI on Vercel!"}
+    return {"message": "Hello from FastAPI on Koyeb!"}
 
 
 @router.post("/upload")
@@ -38,23 +38,24 @@ async def upload_pdf(file: UploadFile = File(...), db: Session = Depends(get_db)
         file_content = await file.read()
 
         # Upload the file to S3
-        s3_client.put_object(Bucket=BUCKET_NAME, Key=file.filename, Body=file_content)
+        s3_client.put_object(Bucket=BUCKET_NAME,
+                             Key=file.filename, Body=file_content)
 
-        # Extract text (you might need to extract it from the uploaded file in S3)
-        # You can download the file again from S3 if needed for processing
-        # Alternatively, you can process the file_content directly if it's a PDF
-
-        # Save metadata to database
-        pdf_doc = PDFDocument(filename=file.filename,upload_date=datetime.now())
+        # Optionally save metadata to the database
+        pdf_doc = PDFDocument(filename=file.filename,
+                              upload_date=datetime.now())
         db.add(pdf_doc)
         db.commit()
         db.refresh(pdf_doc)
 
         return {"filename": file.filename, "id": pdf_doc.id}
+
     except NoCredentialsError:
         raise HTTPException(
-            status_code=403, detail="Credentials not available")
+            status_code=403, detail="AWS credentials not found.")
+    except ClientError as e:
+        raise HTTPException(
+            status_code=500, detail=f"AWS error: {e.response['Error']['Message']}")
     except Exception as e:
-        # Log the exception
         print(f"Error occurred: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
