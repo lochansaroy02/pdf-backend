70049c8cbe116199a7541fbe3c4bb22727a80be3
lochansaroy02 <lochan.kumar.fit.csebct20@teamfuture.in>
2024-11-05 17:34:15 +0530

    fixed cors error

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # cors error fix
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*']
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 
